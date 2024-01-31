e68cbcb00ca02fd0db10e41a2f602c3789bfbc59
ssatocc <ssatocc@gmail.com>
2024-01-31 12:26:49 +0900

    Update send.sh

--- send.sh
+++ send.sh
@@ -5,8 +5,9 @@ IMAGE_DIR="./images/"
 MAX_NUM=`ls $IMAGE_DIR | wc -l`
 RANDOM_NUM=$((RANDOM % MAX_NUM + 1))
 IMAGE_FILE="${IMAGE_DIR}${RANDOM_NUM}.png"
+ENDPOINT="https://notify-api.line.me/api/notify"
 
 curl -X POST -H "Authorization: Bearer $1" \
     -F "message=$MSG" \
     -F "imageFile=@$IMAGE_FILE" \
-    "https://notify-api.line.me/api/notify"
+    $ENDPOINT
